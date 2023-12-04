414217be8f2f2cc0600e42b2b2e0093dce1d30c9
dy102 <ekagnlek2@gmail.com>
2023-12-04 22:16:30 +0900

    feat: 숫자 야구 게임을 진행하는 역할

---
+++ src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,50 @@
+package baseball.controller;
+
+import baseball.domain.Computer;
+import baseball.domain.dto.BaseballResult;
+import baseball.option.GameStatus;
+import baseball.service.BaseballService;
+import baseball.util.GameManager;
+import baseball.view.Input;
+import baseball.view.Output;
+
+public class BaseballController {
+    private BaseballService baseballService;
+
+    public void run() {
+        start();
+        GameStatus gameStatus = GameStatus.RUN;
+        while (gameStatus == GameStatus.RUN) {
+            gameStatus = compareNumbers(baseballService);
+            if (gameStatus == GameStatus.RESTART) {
+                gameStatus = restart();
+            }
+        }
+    }
+
+    private void start() {
+        baseballService = new BaseballService(Computer.makeRandomNumbers());
+        Output.start();
+    }
+
+    private static GameStatus compareNumbers(BaseballService baseballService) {
+        GameStatus gameStatus;
+        BaseballResult baseballResult = baseballService.compareNumbers();
+        Output.result(baseballResult);
+
+        gameStatus = baseballService.isContinue(baseballResult);
+        return gameStatus;
+    }
+
+    private GameStatus restart() {
+        GameStatus gameStatus;
+        Output.end();
+        gameStatus = GameManager.isRestart(Input.restart());
+        if (gameStatus == GameStatus.RUN) {
+            baseballService = new BaseballService(Computer.makeRandomNumbers());
+        }
+        return gameStatus;
+    }
+
+
+}
