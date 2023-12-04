a7507b88e4c8fd4198f3c4b23ef10221815b4f82
dy102 <ekagnlek2@gmail.com>
2023-12-04 22:11:31 +0900

    feat: 각종 게임 옵션들을 관리하는 역할

---
+++ src/main/java/baseball/option/GameOption.java
@@ -0,0 +1,13 @@
+package baseball.option;
+
+public class GameOption {
+    private GameOption() {
+    }
+
+    public static final int NUMBER_LENGTH = 3;
+    public static final int NUMBER_MINIMUM_BOUND = 1;
+    public static final int NUMBER_MAXIMUM_BOUND = 9;
+    public static final int GAME_END_STRIKE_COUNT = 3;
+    public static final int RESTART_NUMBER = 1;
+    public static final int STOP_NUMBER = 2;
+}
